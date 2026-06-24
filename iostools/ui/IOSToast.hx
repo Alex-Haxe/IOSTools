de9de99340efeed867e7fb6b7874e2c93fa35964
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-06-23 21:43:45 -0700

    oh

--- iostools/ui/IOSToast.hx
+++ iostools/ui/IOSToast.hx
@@ -5,7 +5,7 @@ import cpp.Lib;
 #end
 
 @:buildXml('<include name="${haxelib:IOSTools}/project/Build.xml" />')
-class Toast {
+class IOSToast {
     #if ios
     private static var iostools_ui_show_toast = Lib.load("iostools", "iostools_ui_show_toast", 1);
     #end
