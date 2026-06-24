a1a93149d071916114fc1f4af3eb7a6dc38e7c60
Mist <221183655+Misthx@users.noreply.github.com>
2026-06-24 16:13:20 -0700

    mm File

---
+++ dependencies/iostools/Management/IOSBrowser.mm
@@ -0,0 +1,22 @@
+#import <UIKit/UIKit.h>
+#include <hx/CFFI.h>
+#include "IOSBrowser.h"
+
+namespace ios_browser {
+
+    void open_url(const char* url) {
+        NSString *nsUrl = [NSString stringWithUTF8String:url];
+        NSURL *targetUrl = [NSURL URLWithString:nsUrl];
+        
+        if (targetUrl) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] openURL:targetUrl options:@{} completionHandler:nil];
+            });
+        }
+    }
+}
+
+extern "C" void ios_browser_open_url(value url) {
+    ios_browser::open_url(val_string(url));
+}
+val_prime1(ios_browser_open_url);
