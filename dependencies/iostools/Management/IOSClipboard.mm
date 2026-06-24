37b99cde94d77c9df7a2652a2700910a4a23ab01
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-06-24 09:41:57 -0700

    clipboard mm

---
+++ dependencies/iostools/Management/IOSClipboard.mm
@@ -0,0 +1,38 @@
+#import <UIKit/UIKit.h>
+#include <hx/CFFI.h>
+#include "IOSClipboard.h"
+
+namespace ios_clipboard {
+
+    void set_text(const char* text) {
+        NSString *nsText = [NSString stringWithUTF8String:text];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [UIPasteboard generalPasteboard].string = nsText;
+        });
+    }
+
+    const char* get_text() {
+        __block NSString *nsText = @"";
+        if ([NSThread isMainThread]) {
+            nsText = [UIPasteboard generalPasteboard].string;
+        } else {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                nsText = [UIPasteboard generalPasteboard].string;
+            });
+        }
+        if (nsText == nil) {
+            nsText = @"";
+        }
+        return [nsText UTF8String];
+    }
+}
+
+extern "C" void ios_clipboard_set_text(value text) {
+    ios_clipboard::set_text(val_string(text));
+}
+val_prime1(ios_clipboard_set_text);
+
+extern "C" value ios_clipboard_get_text() {
+    return alloc_string(ios_clipboard::get_text());
+}
+val_prime0(ios_clipboard_get_text);
