bf9fb75e8ca3f312e2b05767195ac4e0c54f42d1
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-06-24 09:45:16 -0700

    cool right?

---
+++ iostools/management/IOSClipboard.hx
@@ -0,0 +1,27 @@
+package iostools.management;
+
+#if cpp
+import cpp.Prime;
+#end
+
+class IOSClipboard {
+
+    #if ios
+    private static var ios_clipboard_set_text = Prime._load("iosclipboard", "ios_clipboard_set_text", "vs", false);
+    private static var ios_clipboard_get_text = Prime._load("iosclipboard", "ios_clipboard_get_text", "s", false);
+    #end
+
+    public static function setText(text:String):Void {
+        #if ios
+        ios_clipboard_set_text(text);
+        #end
+    }
+
+    public static function getText():String {
+        #if ios
+        return ios_clipboard_get_text();
+        #else
+        return "";
+        #end
+    }
+}
