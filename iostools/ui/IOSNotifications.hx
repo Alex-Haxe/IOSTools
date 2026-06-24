66fab365b66443545428ca9bd2aa95b022460708
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-06-24 13:36:21 -0700

    notification stuff

---
+++ iostools/ui/IOSNotifications.hx
@@ -0,0 +1,25 @@
+package iostools.ui;
+
+#if cpp
+import cpp.Prime;
+#end
+
+class IOSNotifications {
+
+    #if ios
+    private static var ios_notifications_request_permission = Prime._load("iosnotifications", "ios_notifications_request_permission", "v", false);
+    private static var ios_notifications_send_notification = Prime._load("iosnotifications", "ios_notifications_send_notification", "vss", false);
+    #end
+
+    public static function requestPermission():Void {
+        #if ios
+        ios_notifications_request_permission();
+        #end
+    }
+
+    public static function send(title:String, body:String):Void {
+        #if ios
+        ios_notifications_send_notification(title, body);
+        #end
+    }
+}
