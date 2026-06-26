6bbae9558975fd2ca5caf44b8fe6cadcaea7cd53
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-06-26 08:41:31 -0700

    fuck

--- iostools/storage/IOSFiles.hx
+++ iostools/storage/IOSFiles.hx
@@ -8,13 +8,12 @@ import cpp.Prime;
 #end
 
 @:buildXml('<include name="${haxelib:IOSTools}/project/Build.xml" />')
-    
 class IOSFiles {
     #if ios
-    private static var ios_files_init = Prime._load("iosfiles", "ios_files_init", "vvv", false);
-    private static var ios_files_pick_file = Prime._load("iosfiles", "ios_files_pick_file", "v", false);
-    private static var ios_files_pick_folder = Prime._load("iosfiles", "ios_files_pick_folder", "v", false);
-    private static var ios_files_save = Prime._load("iosfiles", "ios_files_save", "vss", false);
+    private static var ios_files_init = Prime.load("iosfiles", "ios_files_init", "vvv", false);
+    private static var ios_files_pick_file = Prime.load("iosfiles", "ios_files_pick_file", "v", false);
+    private static var ios_files_pick_folder = Prime.load("iosfiles", "ios_files_pick_folder", "v", false);
+    private static var ios_files_save = Prime.load("iosfiles", "ios_files_save", "vss", false);
     #end
 
     public static function setup(onSuccess:String->Void, onCancel:Void->Void):Void {
