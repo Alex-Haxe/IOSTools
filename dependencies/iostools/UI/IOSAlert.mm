573555f528eb2eb9d8373d4ce6062eba8a52e57b
ShotL <261809561+ChanceXML@users.noreply.github.com>
2026-07-03 13:28:42 -0700

    fucking remake
    Co-authored-by: Mist <221183655+Misthx@users.noreply.github.com>

--- dependencies/iostools/UI/IOSAlert.mm
+++ dependencies/iostools/UI/IOSAlert.mm
@@ -1,28 +1,64 @@
 #import <UIKit/UIKit.h>
+#include <hx/CFFI.h>
+#include <hx/CFFIPrime.h>
 #include "IOSAlert.h"
 
-namespace iosalert {
-    void showAlert(const char* title, const char* message, const char* buttonText) {
-        NSString* reqTitle = [NSString stringWithUTF8String:title];
-        NSString* reqMessage = [NSString stringWithUTF8String:message];
-        NSString* reqButton = [NSString stringWithUTF8String:buttonText];
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            UIAlertController* alert = [UIAlertController alertControllerWithTitle:reqTitle
-                                                                           message:reqMessage
-                                                                    preferredStyle:UIAlertControllerStyleAlert];
-            
-            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:reqButton
-                                                                    style:UIAlertActionStyleDefault
-                                                                  handler:nil];
-            
-            [alert addAction:defaultAction];
-            
-            UIViewController* rootVC = [[[UIApplication sharedApplication] keyWindow] rootViewController];
-            while (rootVC.presentedViewController) {
-                rootVC = rootVC.presentedViewController;
+@interface IOSAlertNative : NSObject
++ (void)showAlert:(NSString*)title message:(NSString*)message buttonText:(NSString*)buttonText;
+@end
+
+@implementation IOSAlertNative
+
++ (void)showAlert:(NSString*)title message:(NSString*)message buttonText:(NSString*)buttonText {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+                                                                       message:message
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:buttonText
+                                                                style:UIAlertActionStyleDefault
+                                                              handler:nil];
+        [alert addAction:defaultAction];
+
+        UIViewController *rootVC = nil;
+
+        if (@available(iOS 13.0, *)) {
+            for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive &&
+                    [scene isKindOfClass:[UIWindowScene class]]) {
+                    UIWindowScene *windowScene = (UIWindowScene *)scene;
+                    for (UIWindow *window in windowScene.windows) {
+                        if (window.isKeyWindow) {
+                            rootVC = window.rootViewController;
+                            break;
+                        }
+                    }
+                    if (rootVC != nil) break;
+                }
             }
-            [rootVC presentViewController:alert animated:YES completion:nil];
-        });
-    }
+        }
+
+        if (rootVC == nil) {
+            rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        }
+
+        while (rootVC.presentedViewController) {
+            rootVC = rootVC.presentedViewController;
+        }
+
+        [rootVC presentViewController:alert animated:YES completion:nil];
+    });
 }
+
+@end
+
+extern "C" value ios_show_native_alert(value title, value message, value buttonText) {
+    NSString *nsTitle = [NSString stringWithUTF8String:val_string(title)];
+    NSString *nsMessage = [NSString stringWithUTF8String:val_string(message)];
+    NSString *nsButton = [NSString stringWithUTF8String:val_string(buttonText)];
+
+    [IOSAlertNative showAlert:nsTitle message:nsMessage buttonText:nsButton];
+    return alloc_null();
+}
+
+DEFINE_PRIME3(ios_show_native_alert);
