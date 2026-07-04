6437015de7ac376fa8f7ec56fc4d5151b280fd04
Mist <221183655+Misthx@users.noreply.github.com>
2026-07-03 17:05:39 -0700

    IOS Upload

--- dependencies/iostools/Network/IOSUpload.mm
+++ dependencies/iostools/Network/IOSUpload.mm
@@ -1,31 +1,7 @@
 #include <Foundation/Foundation.h>
-#import "iostools.h"
+#import "IOSUpload.h"
 
 namespace iostools {
-    
-    void downloadFile(const char* urlStr, const char* filenameStr) {
-        NSString *urlString = [NSString stringWithUTF8String:urlStr];
-        NSString *filename = [NSString stringWithUTF8String:filenameStr];
-        
-        NSURL *url = [NSURL URLWithString:urlString];
-        if (!url) return;
-        
-        NSURLSession *session = [NSURLSession sharedSession];
-        NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url
-            completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
-                if (error == nil && location != nil) {
-                    NSFileManager *fileManager = [NSFileManager defaultManager];
-                    NSArray *urls = [fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
-                    NSURL *documentsDirectory = [urls firstObject];
-                    NSURL *destinationURL = [documentsDirectory URLByAppendingPathComponent:filename];
-                    
-                    [fileManager removeItemAtURL:destinationURL error:nil];
-                    [fileManager moveItemAtURL:location toURL:destinationURL error:nil];
-                }
-            }];
-            
-        [downloadTask resume];
-    }
 
     void uploadFile(const char* urlStr, const char* filenameStr) {
         NSString *urlString = [NSString stringWithUTF8String:urlStr];
