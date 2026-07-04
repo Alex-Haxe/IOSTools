42bd5f88349c8f968888d09501f4a31d2de8ea5a
Mist <221183655+Misthx@users.noreply.github.com>
2026-07-03 17:04:55 -0700

    Add IOSUpload.h header file

--- dependencies/iostools/Network/IOSTools.h
+++
@@ -1,9 +0,0 @@
-#ifndef IOSTOOLS_H
-#define IOSTOOLS_H
-
-namespace iostools {
-    void downloadFile(const char* url, const char* filename);
-    void uploadFile(const char* url, const char* filename);
-}
-
-#endif
---
+++ dependencies/iostools/Network/IOSUpload.h
@@ -0,0 +1,8 @@
+#ifndef IOSUPLOAD_H
+#define IOSUPLOAD_H
+
+namespace iostools {
+    void uploadFile(const char* url, const char* filename);
+}
+
+#endif
